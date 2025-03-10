0132e338f457b3ac872e7022ac3165c0233bd795
Jaroslaw <jaroslaw.chaikin@gmail.com>
2025-03-10 22:03:56 +0600

    add lucide icons npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { ArrowDownRight } from "lucide-react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <h1>
+        hello <ArrowDownRight size={"24px"}  />
+      </h1>
     </>
   );
 }
